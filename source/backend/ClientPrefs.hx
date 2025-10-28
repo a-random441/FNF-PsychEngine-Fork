4c77b0434094daaf442730253903f33da673df2f
a-random441 <somerandomgithubacc@gmail.com>
2025-10-28 16:49:34 +0000

    Conditionally set windowColorMode for non-linux

--- source/backend/ClientPrefs.hx
+++ source/backend/ClientPrefs.hx
@@ -13,7 +13,7 @@ import states.TitleState;
 	public var opponentStrums:Bool = true;
 	public var showFPS:Bool = true;
 	public var flashing:Bool = true;
-	public var windowColorMode:String = 'Light';
+	#if !linux public var windowColorMode:String = 'Light'; #end
 	public var autoPause:Bool = true;
 	public var antialiasing:Bool = true;
 	public var noteSkin:String = 'Default';
