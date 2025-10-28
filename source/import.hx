6ca6ee02c2302ff269330d6d0d290dd4a59c75e1
a-random441 <somerandomgithubacc@gmail.com>
2025-10-28 16:44:08 +0000

    Add import for WindowColorMode in import.hx

--- source/import.hx
+++ source/import.hx
@@ -63,5 +63,7 @@ import flixel.group.FlxSpriteGroup;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.addons.transition.FlxTransitionableState;
 
+import hxwindowmode.WindowColorMode;
+
 using StringTools;
 #end
